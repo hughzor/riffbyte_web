b2aa563e6588751d5b3e756bd208a2ac5eddfa8b
hughzor <hughzorinthematrix3@gmail.com>
2026-07-13 16:39:16 +0200

    Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'firebase_options.dart';
 import 'router/app_router.dart';
 import 'services/auth_service.dart';
 import 'theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     ChangeNotifierProvider(
       create: (_) => AuthService(),
